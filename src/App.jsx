ae26bba6df7a9970406715dc5dda87336fa7ad11
Flávio Marques <flaviomarques2@hotmail.com>
2023-07-08 11:51:22 -0300

    inserindo e trabalhando com o componente InputLabel e Input do MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Button, Grid } from '@mui/material';
+import { Button, Grid, Input, InputAdornment, TextField } from '@mui/material';
 import Carousel from './components/praticas/Carrossel';
+import InputLabel from '@mui/material/InputLabel';
+
 
 function App() {
  
@@ -9,6 +11,18 @@ function App() {
       <Grid sx={{display:'flex', justifyContent:'center'}}>
         <Button variant='contained' style={{margin:'auto', border:'none'}}>Botão</Button>
         <Carousel/>
+
+        <TextField>
+          <InputLabel htmlFor='input-com-icone'>
+            Input com ícone
+          </InputLabel>
+            <Input id='input-com-icone' type='text'
+             startAdornment={
+              <InputAdornment position="start">
+  
+              </InputAdornment>
+            }/>        
+        </TextField>
         </Grid>
   );
 }
