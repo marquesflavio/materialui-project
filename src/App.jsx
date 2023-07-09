10e74ae20f10ebe7b570e53918730bc5269c3a04
Flávio Marques <flaviomarques2@hotmail.com>
2023-07-09 14:29:54 -0300

    lib Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
 import './App.css'
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import { Box, Slide } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Slider from 'react-slick';
 
-function App() { 
-
+function App() {
+  
+const Carousel = () => {
+  const config = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+};
   return (
-    <>
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained" color='info'>Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
-        
-    </>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ 
+          bgcolor: '#cecece', 
+          height: '50vh'
+        }} 
+        />
+        Oi na caixa
+      </Container>
+
+        <Slider>
+          <div>
+            <h3>Slide 1</h3>
+          </div>
+          <div>
+            <h3>Slide2</h3>
+          </div>
+          <div>
+            <h3>Slide 3</h3>
+          </div>
+        </Slider>
+
+
+
+    </React.Fragment>
+    
   )
 }
 
